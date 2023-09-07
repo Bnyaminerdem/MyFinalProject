01b947f4ad454dc6ab5b1b1b1b78c884351cad29
Bnyaminerdem <berdemciftci@gmail.com>
2023-09-07 13:06:57 +0300

    Add project files.

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstarct;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //İş Kodları
+           return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryid)
+        {
+           return _categoryDal.Get(C=>C.CategoryId == categoryid);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,98 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product {ProductId=1, CategoryId=1, ProductName ="Bardak", UnitPrice=15,UnitsInStock=15},
+                new Product {ProductId=2, CategoryId=2, ProductName ="Kamera", UnitPrice=500,UnitsInStock=3},
+                new Product {ProductId=3, CategoryId=3, ProductName ="Telefon", UnitPrice=1500,UnitsInStock=2},
+                new Product {ProductId=4, CategoryId=4, ProductName ="Klavye", UnitPrice=150,UnitsInStock=65},
+                new Product {ProductId=5, CategoryId=5, ProductName ="Faree", UnitPrice=85,UnitsInStock=1},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {       
+            Product productToDelete = _products.SingleOrDefault(P=>P.ProductId==product.ProductId);
+            _products.Remove(productToDelete);
+
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(P => P.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
